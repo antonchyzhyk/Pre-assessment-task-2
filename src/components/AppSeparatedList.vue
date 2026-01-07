f80f917035fd4d9faad41f41d7ea7778ccd9381f
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-07 02:37:11 +0200

    feature: create separated list component

---
+++ src/components/AppSeparatedList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-center gap-1">
+    <template v-for="(item, index) in items" :key="index">
+      <slot name="item" :item="item">
+        <span>
+          {{ item }}
+        </span>
+      </slot>
+
+      <slot v-if="index < items.length - 1" name="separator">
+        <span>
+          {{ separator }}
+        </span>
+      </slot>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup generic="T">
+withDefaults(
+  defineProps<{
+    items: T[]
+    separator?: string
+  }>(),
+  {
+    separator: '•'
+  }
+)
+</script>
+
