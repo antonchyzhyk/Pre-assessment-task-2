0477870722e2bccde85d6c837c6b9ca399f67e21
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-07 02:34:42 +0200

    project-setup: remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
-  <ElConfigProvider
-    :locale="{
-      name: locale,
-      el: messages[locale]?.el as TElementPlus['TranslatePair']
-    }"
-  >
+  <ElConfigProvider>
     <router-view />
-    <Modals />
-
-    <ElButton @click="openModal('TestModal')">Open modal</ElButton>
   </ElConfigProvider>
 </template>
 
-<script lang="ts" setup>
-const { messages, locale } = useI18n()
-const { openModal } = useModals()
-</script>
